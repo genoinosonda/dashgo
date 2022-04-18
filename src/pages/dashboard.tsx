6655f1b04e4e82f5bc53f2cefb2a58d631f0d4a1
Sonda <genoino.jr@Gmail.com>
2022-04-18 20:59:49 -0300

    Melhorando dashboard - implantando sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Form/Header";
+import { Sidebar } from "../components/Form/Sidebar";
 
-export default function Dashboard(){
-    return(
-        <Header/>
-    )
+export default function Dashboard() {
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex width="100%" marginY="6" maxWidth="1480" mx="auto" px="6">
+          <Sidebar></Sidebar>
+      </Flex>
+    </Flex>
+  );
 }
