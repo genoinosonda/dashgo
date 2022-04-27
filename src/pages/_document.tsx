87b8e59f12576d6e34a3afd41a3849a4b2296132
Sonda <genoino.jr@Gmail.com>
2022-04-26 21:32:49 -0300

    Corrigindo página _document
    Corrigindo importações dos componentes.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,22 @@
-import Document, { Main, NextScript } from "next/document";
+import Document, { Main, NextScript, Head, Html } from "next/document";
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
-      <html>
-        <head>
+      <Html>
+        <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,700;1,400&display=swap"
             rel="stylesheet"
           />
-        </head>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
